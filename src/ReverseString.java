91bbb57e9383d5d5cb8631eca28e7f31db54b9f2
mukundkhilare19 <mukundkhilare123@gmail.com>
2023-07-18 10:05:48 +0530

    Create Demo123

---
+++ src/ReverseString.java
@@ -0,0 +1,22 @@
+
+
+import java.util.*;
+class ReverseString
+{
+  public static void main(String args[])
+  {
+    String str1, str2 = "";
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter a string to reverse");
+    str1 = sc.nextLine();
+
+    int length = str1.length();
+
+    for (int i = length - 1 ; i >= 0 ; i--)
+      str2 = str2 + str1.charAt(i);
+
+    System.out.println("Reverse of the string: " + str2);
+  }
+  
+}
